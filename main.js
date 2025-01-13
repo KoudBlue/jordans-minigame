0aeb42162c505a4bd70e96ca6149a65fe092d8c7
Sm Kou <66178856+SmKou@users.noreply.github.com>
2025-01-12 23:27:41 -0800

    WIP Add ui and functionality

---
+++ main.js
@@ -0,0 +1,18 @@
+const move_btn = {
+    up: document.getElementById("up-btn"),
+    down: document.getElementById("dn-btn"),
+    left: document.getElementById("lft-btn"),
+    right: document.getElementById("rgt-btn")
+}
+
+/*  Actions
+    1. Confirm / Activate
+       - Ask
+       - Exchange: give / request
+       - Answer
+    2. Cancel
+    3. Menu
+       - Contacts
+       - Inventory
+       - Guide [language manual]
+*/
